de20a05c1d7565596746163b18a6d0d454a14ff4
towienko <01169218@pw.edu.pl>
2025-04-01 01:59:57 +0200

    Add PG creds to secret manager

--- terraform/secretmanager.tf
+++ terraform/secretmanager.tf
@@ -60,10 +60,48 @@ resource "google_secret_manager_secret_iam_binding" "elasticsearch_cacert_elsast
   ]
 }
 
+resource "google_secret_manager_secret" "postgres_root_password" {
+  secret_id = "${local.prfx}postgres-root-password"
+
+  replication {
+    user_managed {
+        replicas {
+            location = local.gcp_region
+        }
+    }
+  }
+}
+
+resource "google_secret_manager_secret_version" "postgres_root_password_initial" {
+  secret = google_secret_manager_secret.postgres_root_password.id
+  secret_data = base64encode(coalesce(var.visit_manager_postgres_root_password, random_password.visit_manager_postgres_generated_password_root[0].result))
+  is_secret_data_base64 = true
+}
+
+resource "google_secret_manager_secret" "postgres_user_password" {
+  secret_id = "${local.prfx}postgres-user-password"
+
+  replication {
+    user_managed {
+        replicas {
+            location = local.gcp_region
+        }
+    }
+  }
+}
+
+resource "google_secret_manager_secret_version" "postgres_user_password_initial" {
+  secret = google_secret_manager_secret.postgres_user_password.id
+  secret_data = base64encode(coalesce(var.visit_manager_postgres_user_password, random_password.visit_manager_postgres_generated_password_user[0].result))
+  is_secret_data_base64 = true
+}
+
 resource "google_secret_manager_secret_iam_binding" "gke_pod_identity_binding" {
   for_each = toset([
     google_secret_manager_secret.elasticsearch_root_password.secret_id,
-    google_secret_manager_secret.elasticsearch_cacert.secret_id
+    google_secret_manager_secret.elasticsearch_cacert.secret_id,
+    google_secret_manager_secret.postgres_root_password.secret_id,
+    google_secret_manager_secret.postgres_user_password.secret_id
   ])
   project = local.gcp_project_id
   secret_id = each.value
