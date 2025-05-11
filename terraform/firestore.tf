319fd0e42a79ccbf2c61fc630e32c693185a3cd3
towienko <01169218@pw.edu.pl>
2025-05-11 16:11:11 +0200

    Add kafka oauth

--- terraform/firestore.tf
+++ terraform/firestore.tf
@@ -1,6 +1,7 @@
 resource "google_firestore_database" "database" {
-  project     = local.gcp_project_id
-  name        = "ersms"
-  location_id = local.gcp_region
-  type        = "FIRESTORE_NATIVE"
+  project         = local.gcp_project_id
+  name            = "ersms"
+  location_id     = local.gcp_region
+  type            = "FIRESTORE_NATIVE"
+  deletion_policy = "DELETE"
 }
